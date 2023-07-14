5d36229e65e09b8fca8e9297122462ae2d5dd4ca
Jaskaran Singh <jaskaran7087065314@gmail.com>
2023-07-14 08:08:56 -0400

    Completed Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import { FaQuoteRight } from 'react-icons/fa';
+import data from './components/data';
+import './components/index.css'
 
 function App() {
+  const [people, setPeople] = useState(data)
+  const [index, setIndex] = useState(0)
+    
+  const checkForlastSlide = (number)=>{
+          if(number > people.length - 1){
+            return 0
+          }
+          if(number < 0){
+            return people.length -1
+          }
+          return number
+  }
+  const prevBtn = ()=>{
+    setIndex(()=>{
+      let newIndex = index + 1
+      return  checkForlastSlide(newIndex)
+    })
+  }
+  const nextBtn = ()=>{
+    setIndex(()=>{
+      let newIndex = index - 1
+      return  checkForlastSlide(newIndex)
+    })
+  }
+  // return checkForlastSlide(newIndex)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section">
+      <div className='title'>
+        <h2>
+         
+          <span>Review</span>
+        </h2>
+      </div>
+      <div className='section-center'>
+
+        {people.map((person,personIndex)=>{
+          const {id, name, image, title , quote} = person
+
+        
+          
+          if(personIndex === index){
+            return(
+              <article key={id} className='active' >
+        
+                  <img src={image} className='person-img' ></img>
+                  <h4>{name}</h4>
+                  <p className='title'>{title}</p>
+                  <p className='text'>{quote}</p>
+                  <FaQuoteRight className='icon'/>
+              </article>
+              )
+          }
+          if(personIndex === index -1){
+            return(
+              <article key={id} className='nextSlide' >
+        
+                  <img src={image} className='person-img' ></img>
+                  <h4>{name}</h4>
+                  <p className='title'>{title}</p>
+                  <p className='text'>{quote}</p>
+                  <FaQuoteRight className='icon'/>
+              </article>
+              )
+          }
+          if(personIndex === index + 1){
+            return(
+              <article key={id} className='nextSlide' >
+        
+                  <img src={image} className='person-img' ></img>
+                  <h4>{name}</h4>
+                  <p className='title'>{title}</p>
+                  <p className='text'>{quote}</p>
+                  <FaQuoteRight className='icon'/>
+              </article>
+              )
+          }
+          console.log(personIndex === index +1)
+          
+          
+           
+          })}
+         <button className='prev'>
+            <FiChevronLeft onClick={prevBtn}></FiChevronLeft>
+      </button>
+      <button className='next'>
+            <FiChevronRight onClick={nextBtn}></FiChevronRight>
+      </button>
+      </div>
+     
+      
+    </section>
   );
 }
 
